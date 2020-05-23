c0d2ae13aa22053138017383227e5a412b2de264
ssml91 <ssmlee10@gmail.com>
2020-05-22 20:14:56 -0700

    added carousel

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -14,13 +14,14 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 //button listeners
 nextBtn.addEventListener('click', () => {
-    
+    if(counter >= carouselImages.length -1) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 
 prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
