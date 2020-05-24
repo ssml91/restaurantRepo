e49f739a1bd8b8dc4347f8a35082bd7afec91c4f
sbmccauley <60712389+sbmccauley@users.noreply.github.com>
2020-05-23 22:19:52 -0700

    added image column to table

--- scripts/table.js
+++ scripts/table.js
@@ -1,12 +1,20 @@
-let sortDirection = false;
-            let storeInfo = [
-                {'address': 'Miramar Store, 97 Gates Road',     'phone' : '(301)378-2630', 'hours': '9:00am-5:00pm'},
-                {'address': 'Mira Mesa Store, 202-B Laffey Circle',   'phone' : '(301)863-6278', 'hours': '6:00am-3:00pm'},
-                {'address': 'Kearny Mesa Store, 1540 D Lovell Ct',   'phone' : '(619)851-4153', 'hours': '4:00pm-11:00pm'},
-                {'address': 'Clairemont Store, 4545 La Jolla Village Square', 'phone' : '(619)453-8562', 'hours': '9:00am-5:00pm'},
-                {'address': 'University City Store, 2267 Lago Ventana',    'phone' : '(858)256-2044', 'hours': '9:00am-5:00pm'},
-                {'address': 'San Diego Store, 1570 Iris Avenue',   'phone' : '(858)256-9651', 'hours': '4:00pm-11:00pm'},
+$(document).ready(function(){
+
+        window.onload = () => {
+                loadTableData(storeInfo);
+            }
+
+    let sortDirection = false;
+    let storeInfo = [
+                { 'image': '/cucumberRoll.png', 'address': 'Miramar Store, 97 Gates Road',     'phone' : '(301)378-2630', 'hours': '9:00am-5:00pm'},
+                {'image': '/tunaRoll.png','address': 'Mira Mesa Store, 202-B Laffey Circle',   'phone' : '(301)863-6278', 'hours': '6:00am-3:00pm'},
+                {'image': '/phillyRoll.png','address': 'Kearny Mesa Store, 1540 D Lovell Ct',   'phone' : '(619)851-4153', 'hours': '4:00pm-11:00pm'},
+                {'image': '/cucumberRoll.png', 'address': 'Clairemont Store, 4545 La Jolla Village Square', 'phone' : '(619)453-8562', 'hours': '9:00am-5:00pm'},
+                {'image': '/tunaRoll.png','address': 'University City Store, 2267 Lago Ventana',    'phone' : '(858)256-2044', 'hours': '9:00am-5:00pm'},
+                {'image': '/phillyRoll.png','address': 'San Diego Store, 1570 Iris Avenue',   'phone' : '(858)256-9651', 'hours': '4:00pm-11:00pm'},
             ];
+
+            
 
             $('#search-input').on('keyup', function(){
                 var value = $(this).val()
@@ -55,15 +63,17 @@ let sortDirection = false;
 
 
             //table data load
-                window.onload = () => {
-                loadTableData(storeInfo);
-            }
+            //     window.onload = () => {
+            //     loadTableData(storeInfo);
+            // }
              function loadTableData(storeInfo) {
                 const tableBody = document.getElementById('tableData');
                 let dataHtml = '';
 
+                
                 for(let info of storeInfo) {
                     dataHtml += `<tr>
+                                    <td>${'<img src="../images' + info.image +'">'}</td>
                                     <td>${info.address}</td>
                                     <td>${info.phone}</td>
                                     <td>${info.hours}</td>
@@ -73,5 +83,6 @@ let sortDirection = false;
 
                 tableBody.innerHTML= dataHtml;
             } 
+});
 
             
