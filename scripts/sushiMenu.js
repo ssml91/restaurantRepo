74d3b52d9b92c4b5e9ac8e3c608f0f4e43b6cb0e
sbmccauley <60712389+sbmccauley@users.noreply.github.com>
2020-05-30 18:41:04 -0700

    adjusted table

--- scripts/sushiMenu.js
+++ scripts/sushiMenu.js
@@ -5,10 +5,10 @@
     
     let storeInfo = [
                 { 'image': '../../images/cucumberRoll.png', 'item': 'California Roll',     'price' : '10.50', 'description': 'avocado, mango, crab'},
-                { 'image': 'https://via.placeholder.com/400x200.png','item': 'Caterpillar Roll ',   'price' : '12.25', 'description': 'avocado, tuna, eel sauce'},
-                { 'image': 'https://via.placeholder.com/400x200.png','item': 'Crunchy Roll',   'price' : '12.95', 'description': 'tempura crusted, salmon'},
-                { 'image': 'https://via.placeholder.com/400x200.png', 'item': 'Eel Roll', 'price' : '10.00', 'description': 'eel, cucumber, jalapeno'},
-                { 'image': 'https://via.placeholder.com/400x200.png','item': 'Shrimp Tempura Roll',    'price' : '10.00', 'description': 'shrimp, daikon'},
+                { 'image': 'https://via.placeholder.com/400x200.png','item': 'Caterpillar Roll ',   'price' : '12.95', 'description': 'avocado, tuna, eel sauce'},
+                { 'image': 'https://via.placeholder.com/400x200.png','item': 'Crunchy Roll',   'price' : '12.50', 'description': 'tempura crusted, salmon'},
+                { 'image': 'https://via.placeholder.com/400x200.png', 'item': 'Eel Roll', 'price' : '10.95', 'description': 'eel, cucumber, jalapeno'},
+                { 'image': 'https://via.placeholder.com/400x200.png','item': 'Shrimp Tempura Roll',    'price' : '10.25', 'description': 'shrimp, daikon'},
                 {'image': 'https://via.placeholder.com/400x200.png','item': 'Spicy Tuna Roll',   'price' : '8.00', 'description': 'tuna, cream cheese, spicy sauce'},
             ];
 
@@ -49,15 +49,20 @@
                 var order = $(this).data('order');
                 var text = $(this).html();
                 text = text.substring(0, text.length -1)
+                var dataType = $(this).data('type');
 
-                console.log('column was clicked', column, order);
+                console.log('column was clicked', column, order, dataType);
+                if(dataType == 'image') {
+                    console.log('your cannot sort this');
+                    return;
 
+                }
               
                 if(order == 'desc') {
                     $(this).data('order', 'asc')
                     $(this).addClass('asc');
                     $(this).removeClass('desc');
-                    if(column == 'price'){
+                    if(dataType == 'number'){
                         storeInfo = storeInfo.sort((a,b) => parseFloat( a[column]) > parseFloat(b[column]) ? 1: -1)
                         console.log('price was clicked');
                     } else {
@@ -72,7 +77,7 @@
                     $(this).data('order', 'desc')
                     $(this).addClass('desc');
                     $(this).removeClass('asc');
-                    if(column == 'price'){
+                    if(dataType == 'number'){
                         storeInfo = storeInfo.sort((a,b) => parseFloat( a[column]) < parseFloat(b[column]) ? 1: -1)
                         console.log('price was clicked');
                     } else {
