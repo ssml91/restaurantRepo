5f2e18142d162994d0101e194041799c379571cf
littlejtawsome <littlejt73@gmail.com>
2020-06-01 07:42:36 -0700

    forms

--- sushibar/scripts/signin.js
+++ sushibar/scripts/signin.js
@@ -2,10 +2,10 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const signcontainer = document.getElementById('signcontainer');
 
-signUpButton.addEventListener('click', () =>
+signUpButton.addEventListener('click', () =>{
     signcontainer.classList.add('right-panel-active')
-);
+});
 
-signInButton.addEventListener('click', () => 
+signInButton.addEventListener('click', () =>{
     signcontainer.classList.remove('right-panel-active')
-);
+} );
