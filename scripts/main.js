bad021e41b9b3bf0d767ca306c277d1bae1edd67
ssml91 <ssmlee10@gmail.com>
2020-06-02 01:42:59 -0700

    update components page and add examples to index

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,12 @@ navBarToggle.addEventListener('click', function() {
 
         mainNav.classList.toggle('active');
 });
+
+// opens coffee site in new window
+function coffeeFunction() {
+        window.open("demos/coffee/blueRoomCoffee.html");
+    }
+// opens sushi site in new window
+function sushiFunction() {
+        window.open("demos/sushi/index_sushibar.html");
+    }
